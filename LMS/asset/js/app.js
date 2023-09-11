2e24472c168e48030cc2edceb6bdb70c89ee2515
Ragavendran-M <raghavendran.m@coderzbot.com>
2023-09-11 18:45:11 +0530

    first

---
+++ LMS/asset/js/app.js
@@ -0,0 +1,54 @@
+
+
+AOS.init();
+
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 3,
+    spaceBetween: 10,
+    autoplay:{
+        delay:2000,
+        disableOnInteraction:false,
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+    
+    
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      720:{
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
+  })
+const swiper1 = new Swiper('.swiper1', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 200,
+    autoplay:{
+        delay:3500,
+   
+    },
+
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+  });
+    
+
+
+  $(document).ready(function(){
+  $(".counter").counterUp({
+  delay:10,  
+  time:1200
+  })})
